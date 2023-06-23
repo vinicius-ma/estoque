a36de1388b495ff663af07174a6104b58e9757be
Vinícius M. Andreghetti <vinicius.andreghetti@gmail.com>
2023-06-22 23:19:53 -0300

    Adding LoggingInerceptor package

--- app/src/main/java/br/com/vinma/estoque/retrofit/EstoqueRetrofit.java
+++ app/src/main/java/br/com/vinma/estoque/retrofit/EstoqueRetrofit.java
@@ -1,6 +1,8 @@
 package br.com.vinma.estoque.retrofit;
 
 import br.com.vinma.estoque.retrofit.service.ProductService;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -9,9 +11,17 @@ public class EstoqueRetrofit {
     private final ProductService productService;
 
     public EstoqueRetrofit() {
+
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build();
+
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://192.168.0.174:8080/")
+                .baseUrl("http://192.168.0.155:8080/")
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
                 .build();
         productService = retrofit.create(ProductService.class);
     }
